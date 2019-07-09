f3ada51a2e28c526dae8ff7ec2e8b70a2f6144f4
daffodilho <46694934+daffodilho@users.noreply.github.com>
2019-07-09 17:40:10 -0400

    adding video

---
+++ js/GOT_master.js
@@ -0,0 +1,28 @@
+(() => {
+	console.log('fired!');
+	
+	//variable stack -> get the shields/sigils first
+	const sigils = document.querySelectorAll(".sigilContainer"),
+		  lightBox = document.querySelector(".lightbox"),
+		  closeButton = document.querySelector(".close-lightbox"),
+		  houseVideo = document.querySelector(".house-video");
+	
+	function popLightBox() {
+		// debug this so far and make sure the event handling works
+		lightBox.classList.add("show-lightbox");
+		
+		houseVideo.play();
+	}
+	
+	function closeLightBox(event) {
+		event.preventDefault();
+		// make the lightbox close
+		lightBox.classList.remove("show-lightbox");
+		houseVideo.currentTime = 0; // rewind the video
+		houseVideo.pause();
+	}
+	
+	sigils.forEach (sigil => sigil.addEventListener("click", popLightBox));
+	closeButton.addEventListener("click", closeLightBox);
+	houseVideo.addEventListener("ended", closeLightBox);
+})();
